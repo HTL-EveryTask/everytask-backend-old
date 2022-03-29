b2fd4bcc7ead1889b90fe330b9cf28b0aedbd731
Mateusz Kaminski <8077@htl.rennweg.at>
2022-03-29 15:15:45 +0200

    Login Prototype

---
+++ src/db_connect/config.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost";
+$port = 3306;
+$username = "root";
+$password = '';
+$database = "every_task_db";
--- src/login.php
+++ src/login.php
@@ -1,5 +1,54 @@
 <?php
+/**
+ * Author: Kaminski
+ * Date: 29.03.2022
+ */
+
+require_once 'db_connect/connect.php';
+
+
+function isEmailValid($email) {
+    //check if email is an array or numeric or boolean or float or a file,directory or an integer
+    if(is_array($email) || is_numeric($email) || is_bool($email) || is_float($email) || is_file($email) || is_dir($email) || is_int($email))
+        return false;
+    else {
+        //remove whitespaces and
+        //convert email to loweercase for processing
+        $email=trim(strtolower($email));
+
+        //regex
+        $pattern = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
+
+        //compare email with regex
+        return (preg_match($pattern, $email) === 1) ? $email : false;
+    }
+}
+
 
 /**
- * Cool KAM Branch
+ * Login
  */
+$data = json_decode(file_get_contents("php://input"));
+$request = $data->request;
+if($request == 1){
+    $username = $data->username;
+    $password = $data->password;
+
+
+    $query = "SELECT * FROM account WHERE username = '" . trim($username) . "'";
+    $data = $connect->prepare( $query );
+    $data->execute();
+
+    foreach ( $data as $row ) {
+        if($row["password"] === $password) {
+            $response[] = array('status'=>1);
+        } else {
+            $response[] = array('status'=>0);
+        }
+    }
+
+    echo json_encode($response);
+    exit;
+}
+
+

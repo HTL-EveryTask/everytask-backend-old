e938ccae3c9ba8f3e391693f67e31bad2414e44a
JuleZ_2k4 <8096@htl.rennweg.at>
2022-04-05 22:19:03 +0200

    axios request finally working :)

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php
 
 namespace Everytask\Backend;
-require_once 'vendor/autoload.php';
 
-if(isset($_POST['action']) && $_POST['action'] == 'login') {
-    echo 'cat';
+use Login;
+
+require_once 'vendor/autoload.php';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("content-type: application/json");
+
+
+//data returned is only for testing purposes (real data is not returned)
+if (isset($_POST['action']) && $_POST['action'] == 'login') {
+    $data = [
+        'email' => $_POST['username'],
+        'password' => $_POST['password']
+    ];
+    echo json_encode(['success' => true, 'data' => $data]);
+    $data1 = new Login($data['email'], $data['password']);
+    if ($data1->checkCredentials()) {
+        echo json_encode(['success' => true, 'data' => $data]);
+    } else {
+        echo json_encode(['success' => false, 'data' => 'cat']);
+    }
 }

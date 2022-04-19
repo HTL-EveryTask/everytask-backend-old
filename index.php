9d5ecd12c1ac1af2d70f8924cd6fb11aee90a038
JuleZ_2k4 <8096@htl.rennweg.at>
2022-04-19 10:22:26 +0200

    comments added

--- index.php
+++ index.php
@@ -8,12 +8,16 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json");
 
+// gets Input from the request
 $phpInput = file_get_contents('php://input');
+
+// Decodes the input
 $POST = json_decode($phpInput, true);
 
+// Checks if the user exists and if the password is correct
 if (isset($POST['action']) && $POST['action'] == 'login') {
     $user = new Login($POST['email'], $POST['password']);
-    if ($user->checkCredentials()){
+    if ($user->checkCredentials()) {
         echo json_encode(array('status' => 'success'));
     } else {
         echo json_encode(array('status' => 'error'));
